582c6598422b06a21d1d874e947bc55bba245e76
doldol123 <toruig59@gmail.com>
2022-10-02 19:18:48 -0700

    create a seperate js file to store all questions and answers

---
+++ js/questions.js
@@ -0,0 +1,82 @@
+let questions = [
+    {
+        question:'Javascript is an _______ language?',
+        choice1:'Object-Oriented',
+        choice2:'Object-Based',
+        choice3:'Procedural',
+        choice4:'None of the above',
+        correct: 'Object-Oriented'
+    },
+    {
+        question:'Which of the following methods can be used to display data in some form using Javascript?',
+        choice1:'document.write()',
+        choice2:'console.log()',
+        choice3:'window.alert',
+        choice4:'All of the above',
+        correct: 'All of the above'
+    },
+    {
+        question: 'How can a datatype be declared to be a constant type?',
+        choice1:'const',
+        choice2:'var',
+        choice3:'let',
+        choice4:'constant',
+        correct: 'const'
+    },
+    {
+        question: 'When an operator’s value is NULL, the typeof returned by the unary operator is:',
+        choice1:'Boolean',
+        choice2:'Undefined',
+        choice3:'Object',
+        choice4:'Integer',
+        correct: 'Object'
+    },
+    {
+        question: 'Which function is used to serialize an object into a JSON string in Javascript?',
+        choice1:'stringify()',
+        choice2:'parse()',
+        choice3:'convert()',
+        choice4:'None of the above',
+        correct: 'stringify()'
+    },
+    {
+        question: 'The 3 basic object attributes in Javascript are:',
+        choice1:'Class, prototype, object\'s parameters',
+        choice2:'Class, prototype, object\'s extensible flag',
+        choice3:'Class, parameters, object\'s extensible flag',
+        choice4:'Classes, Native object, and interfaces and Objext\'s extensible flag',
+        correct: 'Class, prototype, object\'s extensible flag'
+    },
+    {
+        question: 'Which of the following are closures in Javascript?',
+        choice1:'Variables',
+        choice2:'Functions',
+        choice3:'Objects',
+        choice4:'All of the above',
+        correct: 'All of the above'
+    },
+    {
+        question: 'How to stop an interval timer in Javascript?',
+        choice1:'clearInterval',
+        choice2:'clearTimer',
+        choice3:'intervalOver',
+        choice4:'None of the above',
+        correct: 'clearInterval'
+    },
+    {
+        question: 'How do we write a comment in javascript?',
+        choice1:'/* */',
+        choice2:'//',
+        choice3:'#',
+        choice4:'$ $',
+        correct: '//'
+    },
+    {
+        question: 'Which object in Javascript doesn’t have a prototype?',
+        choice1:'Base Object',
+        choice2:'All objects have a prototype',
+        choice3:'None of the objects have a prototype',
+        choice4:'None of the above',
+        correct: 'Base Object'
+    }
+]
