c2cad1a5cbd11b12eae4c8f5c44605b6e980c186
doldol123 <toruig59@gmail.com>
2022-10-02 19:38:13 -0700

    to run the quize, setup timer/generate question and nextquestion function

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var startBtn = document.getElementById("start-btn");
 var startContainerEl = document.getElementById("start-container");
-
-
+var runningQuestion = 0;
+var questionContainerEl =document.getElementById("question-container");
 
 
 
@@ -10,6 +10,29 @@ var startContainerEl = document.getElementById("start-container");
 
 startBtn.addEventListener("click",startQuiz);
 
+
+
+// timer
+function setTimer() {
+
+}
+
+//generate question
+function generateQuestion(){
+
+}
+
+//go to next question
+function nextQuestion(){
+
+}
+
+
+//startQuiz
 function startQuiz() {
     startContainerEl.classList.add("hidden");
+    setTimer();
+    generateQuestion();
+    questionContainerEl.classList.add("hidden");
+    nextQuestion();
 }
