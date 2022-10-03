9aa56ece16b69e100cf134c948c1576500881eb5
doldol123 <toruig59@gmail.com>
2022-10-02 20:21:04 -0700

    try to sovle addeventlistener null

--- js/script.js
+++ js/script.js
@@ -6,10 +6,11 @@ var secondLeft = 10;
 var timeEl = document.getElementById("timer");
 var intlContainerEl = document.getElementById("intl-container");
 var scoreEl = document.getElementById("final-score");
+var questions = document.getElementById("questions");
 
 
 
-startBtn.addEventListener("click",startQuiz);
+
 
 
 
@@ -27,12 +28,43 @@ function setTimer() {
 
 //generate question
 function generateQuestion(){
-
+    if (runningQuestion <=10) {
+        var q = questions[runningQuestion];
+    
+        questions.innerHTML = "<p>"+ q.questions +"</p>";
+    
+        choice1.innerHTML = q.choice1;
+        if (q.choice1 == q.correct) {
+            choice1.dataset.answer = "correct";
+        } else {
+            choice1.dataset.answer = "wrong";
+        }
+        choice2.innerHTML = q.choice2;
+        if (q.choice2 == q.correct) {
+            choice2.dataset.answer = "correct";
+        } else {
+            choice2.dataset.answer = "wrong";
+        }
+        choice3.innerHTML = q.choice3;
+        if (q.choice3 == q.correct) {
+            choice3.dataset.answer = "correct";
+        } else {
+            choice3.dataset.answer = "wrong";
+        }
+        choice4.innerHTML = q.choice4;
+        if (q.choice4 == q.correct) {
+            choice4.dataset.answer = "correct";
+        } else {
+            choice4.dataset.answer = "wrong";
+        }
+        } else {
+            saveScore();
+}
 }
 
 //go to next question
 function nextQuestion(){
-
+    nextQuestion(nextQuestion[runningQuestion + 1]);
 }
 
 //save current user score
@@ -46,8 +78,10 @@ function saveScore(){
 //startQuiz
 function startQuiz() {
     startContainerEl.classList.add("hidden");
+    questionContainerEl.classList.remove("hidden");
     setTimer();
     generateQuestion();
-    questionContainerEl.classList.add("hidden");
     nextQuestion();
 }
+
+startBtn.addEventListener("click",startQuiz);
